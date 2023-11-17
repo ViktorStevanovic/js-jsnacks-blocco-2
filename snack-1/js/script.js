e922e852dc856c0b96504a5f8579515a53ec048f
ViktorStevanovic <stevanovic.lavoro@gmail.com>
2023-11-17 11:49:44 +0100

    snack with while loop

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,10 +1,15 @@
 // Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
 
 const array = []
-const userNumber = array.push(parseInt(prompt('Type a number')))
 
-let sum = sum + parseInt(array[i])
+let sum = 0
 
 while (sum < 50){
-    const userNumber = array.push(parseInt(prompt('Type a number')))
+
+    const userNumber = (parseInt(prompt('Type a number')))
+    if (!isNaN(userNumber)){
+        sum += userNumber;
+        array.push(userNumber)
+    }
+    console.log(sum)
 }
