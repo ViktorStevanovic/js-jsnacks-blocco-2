91fd679628e0bed2b2a48f99354df71c0507a47a
ViktorStevanovic <stevanovic.lavoro@gmail.com>
2023-11-24 12:31:34 +0100

    snack-9 excercise

---
+++ snack-9/js/script.js
@@ -0,0 +1,57 @@
+// Crea 10 oggetti che rappresentano una zucchina, indicandone per ognuno varietà, peso e lunghezza. Calcola quanto pesano tutte le zucchine.
+
+const listaZucchine = [
+
+    {
+        'type':'zucchine',
+        'weigth':'50gr',
+        'length':'2cm'
+    },
+    {
+        'type':'zucchine',
+        'weigth':'70gr',
+        'length':'7cm'
+    },
+    {
+        'type':'zucchine',
+        'weigth':'52gr',
+        'length':'1cm'
+    },
+    {
+        'type':'zucchine',
+        'weigth':'120gr',
+        'length':'9cm'
+    },
+    {
+        'type':'zucchine',
+        'weigth':'30gr',
+        'length':'23cm'
+    },
+    {
+        'type':'zucchine',
+        'weigth':'44gr',
+        'length':'18cm'
+    },
+    {
+        'type':'zucchine',
+        'weigth':'6gr',
+        'length':'13cm'
+    },
+    {
+        'type':'zucchine',
+        'weigth':'67gr',
+        'length':'34cm'
+    },
+    {
+        'type':'zucchine',
+        'weigth':'56gr',
+        'length':'6cm'
+    },
+    {
+        'type':'zucchine',
+        'weigth':'99gr',
+        'length':'29cm'
+    },
+]
+
+console.log(listaZucchine.weigth())
